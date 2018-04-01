12024725fa742b8f3f881bc174f26445050bb649
Adam Swed <adamswed@gmail.com>
2018-04-01 19:56:01 -0400

    ver4 -- Angular Form - Working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ServerService } from './server.service';
 import { Response } from '@angular/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,31 +14,54 @@ export class AppComponent {
   constructor(private serverService: ServerService){}
 
   servers:any[] = [];
-  appName = this.serverService.getAppName();
+  // appName = this.serverService.getAppName();
 
+
+  serverNameProp:string;
+  capacityProp:number;
+  arr:any[] = [];
 
   onGet(){
     this.serverService.getServers()
     .subscribe(
       (servers: any[]) => {
         this.servers = servers;
-        console.log(this.servers);
       },
       (error) => console.log(error)
     );
   }
 
-  onAddServer(name: string) {
-    this.servers.push({
-      name: name,
-      capacity: 50,
-    });
-    this.serverService.storeServers(this.servers.slice(-1)[0])
-    .subscribe(
-      (response) => console.log(response),
-      (error) => console.log(error)
-    );
+
+  onSubmit(form: NgForm) {
+    // console.log(form.value);
+    // if (form.valid) {
+
+      this.arr=[];
+      this.arr.push(form.value);
+
+      console.log(this.arr);
+
+      this.serverService.storeServers(this.arr)
+      .subscribe(
+        (response) => console.log(response),
+        (error) => console.log(error)
+      );
+
+    // }
   }
+
+  // onAddServer(name: string) {
+  //   this.servers.push({
+  //     name: name,
+  //     capacity: 50,
+  //   });
+  //   this.serverService.storeServers(this.servers.slice(-1)) // slice(-1) --> not adding [0] since the data needs to be in array for PHP
+  //   .subscribe(
+  //     (response) => console.log(response),
+  //     (error) => console.log(error)
+  //   );
+  // }
+
     private generateId() {
       return Math.round(Math.random() * 10000);
   }
--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -5,14 +5,11 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ServerService{
-  arr=[];
   constructor(private http:Http){}
-    storeServers(item){
-      this.arr = [];
-      this.arr.push(item);
-
+  
+    storeServers(setData){
         const myHeaders = new Headers({'Content-Type': 'application/json'});
-        return this.http.post('http://localhost:8888/AngularJS/servers/action.php', this.arr,
+        return this.http.post('http://localhost:8888/AngularJS/servers/action.php', setData,
         {headers: myHeaders}
       );
     }
@@ -27,14 +24,14 @@ export class ServerService{
       );
     }
 
-    getAppName(){
-      return this.http.get('https://ng-http-4bca8.firebaseio.com/data/appName.json')
-      .map(
-        ( response: Response ) => {
-          const data = response.json();
-          return data;
-        }
-      );
-    }
+    // getAppName(){
+    //   return this.http.get('https://ng-http-4bca8.firebaseio.com/data/appName.json')
+    //   .map(
+    //     ( response: Response ) => {
+    //       const data = response.json();
+    //       return data;
+    //     }
+    //   );
+    // }
 
 }
